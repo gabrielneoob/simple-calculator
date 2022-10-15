7aa6b5c0cc4676f0da9dda163429c83f7b6f0434
Neoob <gabrielngc@gmail.com>
2022-10-14 23:26:05 -0300

    js

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-const btnNumbers = document.querySelectorAll('.btn-number');
-const display = document.querySelector('.value');
-let operator = '';
-btnNumbers.forEach((number) => {
-  number.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log(number);
-    if (display.innerText.length <= 14) {
-      display.innerText += number.innerText;
-    }
-  })
-})
+const inputText = document.querySelector('#inputext');
+const equal = document.querySelector('#equal');
+const btns = Array.from(document.querySelectorAll('.btn'));
 
-document.querySelector('.clear').addEventListener('click', (e) => {
-  display.innerText = '';
+// functions
+const show = function () {
+  console.log(this)
+  return inputText.value += this.innerText;
+}
+
+const result = () => {
+  try {
+    return inputText.value = eval(inputText.value);
+  }
+
+  catch (err) {
+    inputText.value = '';
+    alert('Inválido')
+  }
+}
+
+const clear = () => {
+  return inputText.value = '';
+}
+
+// events
+equal.addEventListener('click', result);
+btns.forEach((item) => {
+  item.addEventListener('click', show)
 })
+document.querySelector('#clear').addEventListener('click', clear);
